4e4d99b4fca82053d46df743e79b972f3f484042
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2017-09-23 16:10:15 +0200

    add react native client

---
+++ mobile-client/shared/translate.js
@@ -0,0 +1,32 @@
+const langMap = {
+    english: 'en',
+    spanish: 'es',
+    german: 'de',
+    french: 'fr',
+    chinese: 'zh',
+    italian: 'it',
+    korean: 'ko',
+    japanese: 'ja',
+    dutch: 'nl',
+    hindi: 'hi'
+}
+
+const cache = {}
+
+const apiUrls = {
+    translate: 'dawdawd'
+};
+
+export default function translate(term) {
+    const url = `${apiUrls.translate}&q=${term}&source=en&target=${langMap["italian"]}`;
+    return fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log("translate response: ", json);
+            if (json && json.data && json.data.translations && json.data.translations[0] && json.data.translations[0].translatedText) {
+                return json.data.translations[0].translatedText;
+            } else {
+                return false;
+            }
+        });
+}
